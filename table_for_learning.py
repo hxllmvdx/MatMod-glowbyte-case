e998e6123fda5331ba887f539d1b903ea944d864
hxllmvdx <jigoku.tyan@gmail.com>
2025-05-05 12:09:47 +0300

    feat: added catboost_model

--- table_for_learning.py
+++ table_for_learning.py
@@ -1,45 +1,16 @@
 import pandas as pd
+from os import getcwd
 
 
-def process_weather():
-    weather_19 = pd.read_csv('data/weather_data_2019.csv', sep=',').sort_values(by='date').iloc[:, :-2]
-    weather_20 = pd.read_csv('data/weather_data_2020.csv', sep=',').sort_values(by='date').iloc[:, :-2]
+def process_weather(path):
+    weather = pd.read_csv(path, sep=',').sort_values(by='date').iloc[:, :-2]
 
-    weather_19['date'] = weather_19['date'].apply(lambda x: x.split(' ')[0])
-    weather_20['date'] = weather_20['date'].apply(lambda x: x.split(' ')[0])
+    weather['date'] = weather['date'].apply(lambda x: x.split(' ')[0])
 
     data = {}
 
-    for date in weather_19['date'].unique():
-        temp = weather_19[weather_19['date'] == date]
-
-        temp_max = temp['t'].max()
-        temp_mean = temp['t'].mean()
-
-        humidity_max = temp['humidity'].max()
-        humidity_mean = temp['humidity'].mean()
-
-        precipitation = temp['precipitation'].max()
-
-        v_mean = temp['v_avg'].mean()
-        v_max = temp['v_max'].mean()
-
-        p_mean = temp['p'].mean()
-        p_max = temp['p'].max()
-
-        data['date'] = data.get('date', []) + [date]
-        data['temp_max'] = data.get('temp_max', []) + [temp_max]
-        data['temp_mean'] = data.get('temp_mean', []) + [temp_mean]
-        data['humidity_max'] = data.get('humidity_max', []) + [humidity_max]
-        data['humidity_mean'] = data.get('humidity_mean', []) + [humidity_mean]
-        data['precipitation'] = data.get('precipitation', []) + [precipitation]
-        data['v_mean'] = data.get('v_mean', []) + [v_mean]
-        data['v_max'] = data.get('v_max', []) + [v_max]
-        data['p_mean'] = data.get('p_mean', []) + [p_mean]
-        data['p_max'] = data.get('p_max', []) + [p_max]
-
-    for date in weather_20['date'].unique():
-        temp = weather_20[weather_20['date'] == date]
+    for date in weather['date'].unique():
+        temp = weather[weather['date'] == date]
 
         temp_max = temp['t'].max()
         temp_mean = temp['t'].mean()
@@ -69,8 +40,16 @@ def process_weather():
     return pd.DataFrame(data)
 
 
-def process_fires():
-    fires = pd.read_csv('data/fires.csv', sep=';').sort_values(by='Дата начала')
+def process_fires(path):
+    try:
+        fires = pd.read_csv(path, sep=',').sort_values(by='Дата начала')
+    except:
+        pass
+
+    try:
+        fires = pd.read_csv(path, sep=';').sort_values(by='Дата начала')
+    except:
+        pass
 
     fires['obj_type'] = fires['Склад'].astype(str) + '_' + fires['Штабель'].astype(str)
 
@@ -85,8 +64,8 @@ def process_fires():
     return fires
 
 
-def process_temperature():
-    temperature = pd.read_csv('data/temperature.csv').sort_values(by='Дата акта')
+def process_temperature(path):
+    temperature = pd.read_csv(path).sort_values(by='Дата акта')
 
     temperature['obj_type'] = temperature['Склад'].astype(str) + '_' + temperature['Штабель'].astype(str)
 
@@ -101,8 +80,18 @@ def process_temperature():
     return temperature
 
 
-def merge_tables():
-    weather, fires, temperature = process_weather(), process_fires(), process_temperature()
+def merge_tables(*paths):
+    weathers = []
+    paths = paths[0]
+
+    for path in paths:
+        if 'weather' in path:
+            paths = paths[paths.index(path) + 1:]
+            weathers.append(process_weather(path))
+
+    weather = pd.concat(weathers, axis=0)
+
+    fires, temperature = process_fires(paths[0]), process_temperature(paths[1])
 
     filter_date = temperature['date'].min()
 
@@ -159,9 +148,7 @@ def merge_tables():
     return pd.DataFrame(data).sort_values(by=['obj_type', 'date']).drop_duplicates()
 
 
-def numerate():
-    df = merge_tables()
-
+def numerate(df):
     # Преобразование даты в datetime и сортировка
     df['date'] = pd.to_datetime(df['date'])
     df = df.sort_values(by=['obj_type', 'date']).reset_index(drop=True)
@@ -176,8 +163,7 @@ def numerate():
     return df
 
 
-def delete_less_than_3():
-    df = numerate()
+def delete_less_than_3(df):
     df['date'] = pd.to_datetime(df['date'])
     df = df.sort_values(by=['obj_type', 'date'])
     df['group'] = (df.groupby('obj_type')['date'].diff().dt.days.ne(1)).cumsum()
@@ -192,9 +178,7 @@ def delete_less_than_3():
     return df
 
 
-def shift_target():
-    df = delete_less_than_3()
-
+def shift_target(df):
     df['date'] = pd.to_datetime(df['date'])
 
     # Шаг 1: Создание групп для каждого объекта с учетом разрывов в датах и target=1
@@ -213,6 +197,18 @@ def shift_target():
     df = df.drop(columns=['date_diff', 'gap'])
 
     return df
+
+
+# Создаем лаги внутри каждой группы (obj_type + group) и заполняем пропуски средним по группе
+def create_lag_with_fill(df, column, lag=1):
+    return (
+        df.groupby(['obj_type', 'group'])[column]
+        .shift(lag)
+        .fillna(
+            df.groupby(['obj_type', 'group'])[column]
+            .transform('mean')
+        )
+    )
 
 
 def add_features(df):
@@ -241,7 +237,78 @@ def add_features(df):
 
     df['coal_temp_p_mean'] = df['coal_temp'] * df['p_mean']
 
+    # Пример: добавляем лаги температуры угля за 1 и 3 дня
+    df['coal_temp_lag1d'] = create_lag_with_fill(df, 'coal_temp', lag=1)
+    df['coal_temp_lag2d'] = create_lag_with_fill(df, 'coal_temp', lag=2)
+    df['coal_temp_lag3d'] = create_lag_with_fill(df, 'coal_temp', lag=3)
+
     return df
 
 
-add_features(shift_target()).to_csv('data/learning_table.csv', sep=';', index=False)
+def create_predict_table(*paths):
+    weathers = []
+
+    for path in paths:
+        if 'weather' in path:
+            paths = paths[paths.index(path) + 1:]
+            weathers.append(process_weather(path))
+
+    weather = pd.concat(weathers, axis=0)
+    temperature = process_fires(paths[0])
+
+    begin_date = temperature['date'].min()
+    end_date = temperature['date'].max()
+
+    weather = weather.loc[weather.date >= begin_date]
+    weather = weather.loc[weather.date <= end_date]
+
+    data = {}
+
+    for i in range(3413):
+        temp1 = temperature.iloc[i]
+        temp2 = temperature[temperature['date'] == temp1['date']]
+        temp2 = temp2[temp2['obj_type'] == temp1['obj_type']]
+        temp3 = weather[weather['date'] == temp1['date']]
+
+        data['date'] = data.get('date', []) + [temp1['date']]
+        data['obj_type'] = data.get('obj_type', []) + [temp1['obj_type']]
+        data['coal_temp'] = data.get('coal_temp', []) + [temp2['coal_temp'].max()]
+        data['temp_max'] = data.get('temp_max', []) + [temp3['temp_max'].values[0]]
+        data['temp_mean'] = data.get('temp_mean', []) + [temp3['temp_mean'].values[0]]
+        data['humidity_max'] = data.get('humidity_max', []) + [temp3['humidity_max'].values[0]]
+        data['humidity_mean'] = data.get('humidity_mean', []) + [temp3['humidity_mean'].values[0]]
+        data['precipitation'] = data.get('precipitation', []) + [temp3['precipitation'].values[0]]
+        data['v_mean'] = data.get('v_mean', []) + [temp3['v_mean'].values[0]]
+        data['v_max'] = data.get('v_max', []) + [temp3['v_max'].values[0]]
+        data['p_mean'] = data.get('p_mean', []) + [temp3['p_mean'].values[0]]
+        data['p_max'] = data.get('p_max', []) + [temp3['p_max'].values[0]]
+
+        month = temp1['date'].split('-')[1]
+
+        if month == '05':
+            data['spring'] = data.get('spring', []) + [1]
+            data['summer'] = data.get('summer', []) + [0]
+            data['autumn'] = data.get('autumn', []) + [0]
+        elif month in ['06', '07', '08']:
+            data['spring'] = data.get('spring', []) + [0]
+            data['summer'] = data.get('summer', []) + [1]
+            data['autumn'] = data.get('autumn', []) + [0]
+        else:
+            data['spring'] = data.get('spring', []) + [0]
+            data['summer'] = data.get('summer', []) + [0]
+            data['autumn'] = data.get('autumn', []) + [1]
+
+    df = pd.DataFrame(data)
+    df = numerate(df)
+    df = delete_less_than_3(df)
+    df = add_features(df)
+
+    df.to_csv('data/pred_table.table.csv')
+
+
+def create_learning_table(*paths):
+    df = add_features(shift_target(delete_less_than_3(numerate(merge_tables(paths)))))
+
+    df.to_csv('data/learning_table.csv', index=False)
+
+
